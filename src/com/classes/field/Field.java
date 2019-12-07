6b8e4967b624e9c5194db7a938795805841b900d
ivanhrabcak <39710116+ivanhrabcak@users.noreply.github.com>
2019-12-07 21:50:38 +0100

    Replace lists with arrays??

--- src/com/classes/field/Field.java
+++ src/com/classes/field/Field.java
@@ -7,30 +7,19 @@ public class Field {
     public short[] fieldSize;
     public short treasures;
     public int[] playerPosition;
-    private List<Integer> treasurePositions; // TODO: Make it an array
-    private List<List<Integer>> field; // TODO: Make it an array
+    private int[] treasurePositions; // TODO: Make it an array
+    private short[][] field; // TODO: Make it an array
 
     public Field(short[] fieldSize, short treasures, int[] playerPosition) {
         this.fieldSize = fieldSize;
         this.treasures = treasures;
         this.playerPosition = playerPosition;
-        this.treasurePositions = new ArrayList<>();
+        this.treasurePositions = new int[treasurePositions.length];
         this.field = this.generateField();
     }
 
-    private List<List<Integer>> generateField() {
-        short sizeX = this.fieldSize[0];
-        short sizeY = this.fieldSize[1];
-        List<List<Integer>> l = new ArrayList<>();
-        for (short a = 0; a <= sizeX; a++) {
-            List<Integer> b = new ArrayList<>();
-            for (short c = 0; c <= sizeY; c++) {
-                b.add(0);
-            }
-            l.add(b);
-
-        }
-        // print field
+    private short[][] generateField() {
+        short[][] l = new short[this.fieldSize[0]][this.fieldSize[1]]
         return l;
 
     }
