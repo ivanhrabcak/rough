40b26f2df3c2a515b54f85cf8c9d2df153113b0d
Ivan Hrabcak <ivan.hrabcak@gmail.com>
2019-12-08 21:15:58 +0100

    draw, generateWalls

--- src/com/classes/Field.java
+++ src/com/classes/Field.java
@@ -3,13 +3,17 @@ package com.classes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
+/*
+ *
+ */
 public class Field {
     public short[] fieldSize;
     public short treasures;
     public int[] playerPosition;
-    private int[] treasurePositions; // TODO: Make it an array
-    private short[][] field; // TODO: Make it an array
+    private int[] treasurePositions;
+    private short[][] field;
 
     public Field(short[] fieldSize, short treasures, int[] playerPosition) {
         this.fieldSize = fieldSize;
@@ -21,19 +25,67 @@ public class Field {
 
     private short[][] generateField() {
         short[][] l = new short[this.fieldSize[0]][this.fieldSize[1]];
-        short[] a = new short[this.fieldSize[0]];
-        for (int i = 0; i < this.fieldSize[0]; i++) {
-            a[i] = 0;
+        short[] b = new short[this.fieldSize[1]];
+        for (short x = 0; x < this.fieldSize[0]; x++) {
+            //b = new short[this.fieldSize[1]];
+            for (short y = 0; y < this.fieldSize[1]; y++) {
+                b[y] = 0;
+            }
+            l[x] = b;
         }
-        for (int i = 0; i < this.fieldSize[1]; i++) {
-            l[i] = a;
-        }
+        l = this.generateWalls(l);
+        //this.generateTreasures(l);
+
         return l;
     }
+
+    private void draw() {
+        this.printField();
+        for (int a = 0; a < this.fieldSize[0]; a++) {
+            for (int b = 0; b < this.fieldSize[1]; b++) {
+                short f = this.field[a][b];
+                if (f == -1) {
+                    System.out.print(". ");
+                } else if (f == 1) {
+                    System.out.print("@ ");
+                } else if (f == 0) {
+                    System.out.print("! ");
+                } else {
+                    System.out.print("T ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public short[][] generateWalls(short[][] field) {
+        Random random = new Random(1);
+        short[][] newField = new short[this.fieldSize[0]][this.fieldSize[1]];
+        for (int a = 0; a < this.fieldSize[0]; a++) {
+
+            for (int b = 0; b < this.fieldSize[1]; b++) {
+                int n = random.nextInt(3);
+                if (n == 0) {
+                    field[a][b] = -1;
+                }
+                else {
+                    field[a][b] = (short) n;
+                }
+            }
+        }
+        for (int a = 0; a < this.fieldSize[0]; a++){
+            for (int b = 0; b < this.fieldSize[1]; b++) {
+            }
+        }
+        System.out.println(Arrays.deepToString(field));
+        return field;
+    }
+
 
     public void printField() {
         System.out.println(Arrays.deepToString(this.field));
     }
+
     public boolean move(short[] position) {
         System.out.println(":(((");
         return true;
